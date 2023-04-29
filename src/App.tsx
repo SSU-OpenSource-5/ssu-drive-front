4d8c21cc3f6954b8fd5fc682886fcea1b75000b0
ballsona <ballsona0925@naver.com>
2023-04-29 18:57:44 +0900

    HGD-3 feat: add router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import DrivingPage from './pages/DrivingPage';
+import { Container } from './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '/drive',
+    element: <DrivingPage />,
+  },
+]);
+
 function App() {
-  return <div className="App">Hello, ssu-drive</div>;
+  return (
+    <div className={`App ${Container}`}>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
