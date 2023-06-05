4c4aa3609e098253bc4c1bce2eb96276c6f3dcdb
jungeol-h <dali2728@gmail.com>
2023-06-05 18:50:46 +0900

    EditPage에 BottomNavBar 추가

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import HorizontalLinearStepper from './components/Stepper';
 import { COLORS } from '../../constants/styles/color';
+import BottomNavbar from '../../components/BottomNavbar';
 const steps = [
   'Select campaign settings',
   'Create an ad group',
@@ -13,6 +14,7 @@ export default function EditPage() {
       {/* TODO Padding으로 높이조절 하지 말고 100vh로 높이 조절 */}
       <Box bgcolor={COLORS.grayscale.gray1} padding={2} paddingBottom={100}>
         <HorizontalLinearStepper></HorizontalLinearStepper>
+        <BottomNavbar />
       </Box>
     </>
   );
