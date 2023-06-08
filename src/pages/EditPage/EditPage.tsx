070679628f92c3849a0c111c18bfe60074ba22b2
jungeol-h <dali2728@gmail.com>
2023-06-08 14:34:24 +0900

    페이지 최소 높이 재지정

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -15,7 +15,12 @@ export default function EditPage() {
     <>
       {/* TODO Padding으로 높이조절 하지 말고 100vh로 높이 조절 */}
       {/* <Appbar /> */}
-      <Box bgcolor={COLORS.grayscale.gray1} padding={2} paddingBottom={100}>
+      <Box
+        bgcolor={COLORS.grayscale.gray1}
+        padding={2}
+        paddingBottom={1}
+        minHeight={'100dvh'}
+      >
         <Stepper></Stepper>
         <BottomNavbar />
       </Box>
