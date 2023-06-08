5eb58487996e28cadebded9131227b1bb3d83193
jungeol-h <dali2728@gmail.com>
2023-06-08 13:11:44 +0900

    Appbar 추가

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 import HorizontalLinearStepper from './components/Stepper';
 import { COLORS } from '../../constants/styles/color';
 import BottomNavbar from '../../components/BottomNavbar';
+import Appbar from '../../components/Appbar';
 const steps = [
   'Select campaign settings',
   'Create an ad group',
@@ -12,6 +13,7 @@ export default function EditPage() {
   return (
     <>
       {/* TODO Padding으로 높이조절 하지 말고 100vh로 높이 조절 */}
+      <Appbar />
       <Box bgcolor={COLORS.grayscale.gray1} padding={2} paddingBottom={100}>
         <HorizontalLinearStepper></HorizontalLinearStepper>
         <BottomNavbar />
